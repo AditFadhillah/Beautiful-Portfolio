8f2fcc19b11cb3ab42adbf2d53c83caa43c80a13
AditFadhillah <fadhillah.adit@gmail.com>
2025-08-02 22:27:53 +0200

    Deploy to GitHub

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,12 +8,12 @@ export const HeroSection = () => {
     <div className="container max-2-4xl mx-auto text-center z-10">
       <div className="space-y-6">
         <img
-            src="/croppedCVimage.png"
+            src="/Fadhillah-Portfolio/croppedCVimage.png"
             alt="Aditya Fadhillah"
             className="mx-auto rounded-full w-50 h-50 object-cover mb-4 shadow-lg border-4 border-primary"  
           />
         <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
-          <span classname="opacity-0 animate-fade-in"> Hey, I'm</span>
+          <span className="opacity-0 animate-fade-in"> Hey, I'm</span>
           <span className="text-primary opacity-0 animate-fade-in-delay-1"> 
             {" "}
             Aditya 
